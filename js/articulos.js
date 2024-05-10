8f6dcbcf3a5e571d1deda6e40e8a56b20e2506b9
komargustavo <56453621+komargustavo@users.noreply.github.com>
2024-05-10 20:01:08 -0300

    galeria

--- js/articulos.js
+++ js/articulos.js
@@ -1,106 +1,107 @@
 let principal = document.getElementById("grilla");
 
-mates = new Array();
-(mates[0] = ["/img/imagenes/Mate_10.jpg", "mate 1"]),
-(mates[1] = ["/img/imagenes/Mate_1.jpg", "mate 2"]),
-(mates[2] = ["/img/imagenes/Mate_2.jpg", "mate 3"]),
-(mates[3] = ["/img/imagenes/Mate_3.jpg", "mate 4"]),
-(mates[4] = ["/img/imagenes/Mate_4.jpg", "mate 5"]),
-(mates[5] = ["/img/imagenes/Mate_5.jpg", "mate 6"]),
-(mates[6] = ["/img/imagenes/Mate_6.jpg", "mate 7"]),
-(mates[7] = ["/img/imagenes/Mate_7.jpg", "mate 8"]),
-(mates[8] = ["/img/imagenes/Mate_8.jpg", "mate 9"]),
-(mates[9] = ["/img/imagenes/Mate_9.jpg", "mate 10"]),
-(mates[10] = ["/img/imagenes/Mate_10.jpg", "mate 11"]),
-(mates[11] = ["/img/imagenes/Mate_11.jpg", "mate 12"]),
-(mates[12] = ["/img/imagenes/Mate_12.jpg", "mate 13"]),
-(mates[13] = ["/img/imagenes/Mate_13.jpg", "mate 14"]),
-(mates[14] = ["/img/imagenes/Mate_14.jpg", "mate 15"]),
-(mates[15] = ["/img/imagenes/Mate_15.jpg", "mate 16"]),
-(mates[16] = ["/img/imagenes/Mate_16.jpg", "mate 16"]),
-(mates[17] = ["/img/imagenes/Mate_17.jpg", "mate 17"]),
-(mates[18] = ["/img/imagenes/Mate_18.jpg", "mate 18"]),
-(mates[19] = ["/img/imagenes/Mate_19.jpg", "mate 19"]),
-(mates[20] = ["/img/imagenes/Mate_20.jpg", "mate 20"]),
-(mates[21] = ["/img/imagenes/Mate_21.jpg", "mate 21"]),
-(mates[22] = ["/img/imagenes/Mate_22.jpg", "mate 22"]),
-(mates[23] = ["/img/imagenes/Mate_23.jpg", "mate 23"]),
-(mates[24] = ["/img/imagenes/Mate_24.jpg", "mate 24"]),
-(mates[25] = ["/img/imagenes/Mate_25.jpg", "mate 25"]),
-(mates[26] = ["/img/imagenes/Mate_26.jpg", "mate 26"]),
-(mates[27] = ["/img/imagenes/Mate_27.jpg", "mate 27"]),
-(mates[28] = ["/img/imagenes/Mate_28.jpg", "mate 28"]),
-(mates[29] = ["/img/imagenes/Mate_29.jpg", "mate 29"]),
-(mates[30] = ["/img/imagenes/Mate_30.jpg", "mate 30"]),
-(mates[31] = ["/img/imagenes/Mate_31.jpg", "mate 31"]),
-(mates[32] = ["/img/imagenes/Mate_32.jpg", "mate 32"]),
-(mates[33] = ["/img/imagenes/Mate_33.jpg", "mate 33"]),
-(mates[34] = ["/img/imagenes/Mate_34.jpg", "mate 34"]),
-(mates[35] = ["/img/imagenes/Mate_35.jpg", "mate 35"]),
-(mates[36] = ["/img/imagenes/Mate_36.jpg", "mate 36"]),
-(mates[37] = ["/img/imagenes/Mate_37.jpg", "mate 37"]),
-(mates[38] = ["/img/imagenes/Mate_38.jpg", "mate 38"]),
-(mates[39] = ["/img/imagenes/Mate_39.jpg", "mate 39"]),
-(mates[40] = ["/img/imagenes/Mate_40.jpg", "mate 40"]),
-(mates[41] = ["/img/imagenes/Mate_41.jpg", "mate 41"]),
-(mates[42] = ["/img/imagenes/Mate_42.jpg", "mate 42"]),
-(mates[43] = ["/img/imagenes/Mate_43.jpg", "mate 43"]),
-(mates[44] = ["/img/imagenes/Mate_44.jpg", "mate 44"]),
-(mates[45] = ["/img/imagenes/Mate_45.jpg", "mate 45"]),
-(mates[46] = ["/img/imagenes/Mate_46.jpg", "mate 46"]),
-(mates[47] = ["/img/imagenes/Mate_47.jpg", "mate 47"]),
-(mates[48] = ["/img/imagenes/Mate_48.jpg", "mate 48"]),
-(mates[49] = ["/img/imagenes/Mate_49.jpg", "mate 48"]),
-(mates[50] = ["/img/imagenes/Mate_50.jpg", "mate 49"]),
-(mates[51] = ["/img/imagenes/Mate_51.jpg", "mate 49"]),
-(mates[52] = ["/img/imagenes/Mate_52.jpg", "mate 50"]),
-(mates[53] = ["/img/imagenes/Mate_53.jpg", "mate 51"]),
-(mates[54] = ["/img/imagenes/Mate_54.jpg", "mate 52"]),
-(mates[55] = ["/img/imagenes/Mate_55.jpg", "mate 53"]),
-(mates[56] = ["/img/imagenes/Mate_56.jpg", "mate 54"]),
-(mates[57] = ["/img/imagenes/Mate_57.jpg", "mate 55"]),
-(mates[58] = ["/img/imagenes/Mate_58.jpg", "mate 56"]),
-(mates[59] = ["/img/imagenes/Mate_59.jpg", "mate 57"]),
-(mates[60] = ["/img/imagenes/Mate_60.jpg", "mate 58"]),
-(mates[61] = ["/img/imagenes/Mate_61.jpg", "mate 59"]),
-(mates[62] = ["/img/imagenes/Mate_62.jpg", "mate 60"]),
-(mates[63] = ["/img/imagenes/Mate_63.jpg", "mate 61"]),
-(mates[64] = ["/img/imagenes/Mate_64.jpg", "mate 62"]),
-(mates[65] = ["/img/imagenes/Mate_65.jpg", "mate 63"]),
-(mates[66] = ["/img/imagenes/Mate_66.jpg", "mate 64"]),
-(mates[67] = ["/img/imagenes/Mate_67.jpg", "mate 65"]),
-(mates[68] = ["/img/imagenes/Mate_68.jpg", "mate 66"]),
-(mates[69] = ["/img/imagenes/Mate_69.jpg", "mate 67"]),
-(mates[70] = ["/img/imagenes/Mate_70.jpg", "mate 68"]),
-(mates[71] = ["/img/imagenes/Mate_71.jpg", "mate 69"]),
-(mates[72] = ["/img/imagenes/Mate_72.jpg", "mate 70"]),
-(mates[73] = ["/img/imagenes/Mate_73.jpg", "mate 71"]),
-(mates[74] = ["/img/imagenes/Mate_74.jpg", "mate 72"]),
-(mates[75] = ["/img/imagenes/Mate_75.jpg", "mate 73"]),
-(mates[76] = ["/img/imagenes/Mate_76.jpg", "mate 74"]),
-(mates[77] = ["/img/imagenes/Mate_77.jpg", "mate 75"]),
-(mates[78] = ["/img/imagenes/Mate_78.jpg", "mate 76"]),
-(mates[79] = ["/img/imagenes/Mate_79.jpg", "mate 77"]),
-(mates[80] = ["/img/imagenes/Mate_80.jpg", "mate 78"]),
-(mates[81] = ["/img/imagenes/Mate_81.jpg", "mate 79"]),
-(mates[82] = ["/img/imagenes/Mate_82.jpg", "mate 80"]),
-(mates[83] = ["/img/imagenes/Mate_83.jpg", "mate 81"]),
-(mates[84] = ["/img/imagenes/Mate_84.jpg", "mate 82"]),
-(mates[85] = ["/img/imagenes/Mate_85.jpg", "mate 83"]);
+const mates =[
+    { imagen:"/img/imagenes/Mate_0.jpg", nombre:"mate 1"},
+    { imagen:"/img/imagenes/Mate_1.jpg", nombre:"mate 2"},
+    { imagen:"/img/imagenes/Mate_2.jpg", nombre:"mate 3"},
+    { imagen:"/img/imagenes/Mate_3.jpg", nombre:"mate 4"},
+    { imagen:"/img/imagenes/Mate_4.jpg", nombre:"mate 5"},
+    { imagen:"/img/imagenes/Mate_5.jpg", nombre:"mate 6"},
+    { imagen:"/img/imagenes/Mate_6.jpg", nombre:"mate 7"},
+    { imagen:"/img/imagenes/Mate_7.jpg", nombre:"mate 8"},
+    { imagen:"/img/imagenes/Mate_8.jpg", nombre:"mate 9"},
+    { imagen:"/img/imagenes/Mate_9.jpg", nombre:"mate 10"},
+    { imagen:"/img/imagenes/Mate_10.jpg", nombre: "mate 11"},
+    { imagen:"/img/imagenes/Mate_11.jpg", nombre: "mate 12"},
+    { imagen:"/img/imagenes/Mate_12.jpg", nombre: "mate 13"},
+    { imagen:"/img/imagenes/Mate_13.jpg", nombre: "mate 14"},
+    { imagen:"/img/imagenes/Mate_14.jpg", nombre: "mate 15"},
+    { imagen:"/img/imagenes/Mate_15.jpg", nombre: "mate 16"},
+    { imagen:"/img/imagenes/Mate_16.jpg", nombre: "mate 16"},
+    { imagen:"/img/imagenes/Mate_17.jpg", nombre: "mate 17"},
+    { imagen:"/img/imagenes/Mate_18.jpg", nombre: "mate 18"},
+    { imagen:"/img/imagenes/Mate_19.jpg", nombre: "mate 19"},
+    { imagen:"/img/imagenes/Mate_20.jpg", nombre: "mate 20"},
+    { imagen:"/img/imagenes/Mate_21.jpg", nombre: "mate 21"},
+    { imagen:"/img/imagenes/Mate_22.jpg", nombre: "mate 22"},
+    { imagen:"/img/imagenes/Mate_23.jpg", nombre: "mate 23"},
+    { imagen:"/img/imagenes/Mate_24.jpg", nombre: "mate 24"},
+    { imagen:"/img/imagenes/Mate_25.jpg", nombre: "mate 25"},
+    { imagen:"/img/imagenes/Mate_26.jpg", nombre: "mate 26"},
+    { imagen:"/img/imagenes/Mate_27.jpg", nombre: "mate 27"},
+    { imagen:"/img/imagenes/Mate_28.jpg", nombre: "mate 28"},
+    { imagen:"/img/imagenes/Mate_29.jpg", nombre: "mate 29"},
+    { imagen:"/img/imagenes/Mate_30.jpg", nombre: "mate 30"},
+    { imagen:"/img/imagenes/Mate_31.jpg", nombre: "mate 31"},
+    { imagen:"/img/imagenes/Mate_32.jpg", nombre: "mate 32"},
+    { imagen:"/img/imagenes/Mate_33.jpg", nombre: "mate 33"},
+    { imagen:"/img/imagenes/Mate_34.jpg", nombre: "mate 34"},
+    { imagen:"/img/imagenes/Mate_35.jpg", nombre: "mate 35"},
+    { imagen:"/img/imagenes/Mate_36.jpg", nombre: "mate 36"},
+    { imagen:"/img/imagenes/Mate_37.jpg", nombre: "mate 37"},
+    { imagen:"/img/imagenes/Mate_38.jpg", nombre: "mate 38"},
+    { imagen:"/img/imagenes/Mate_39.jpg", nombre: "mate 39"},
+    { imagen:"/img/imagenes/Mate_40.jpg", nombre: "mate 40"},
+    { imagen:"/img/imagenes/Mate_41.jpg", nombre: "mate 41"},
+    { imagen:"/img/imagenes/Mate_42.jpg", nombre: "mate 42"},
+    { imagen:"/img/imagenes/Mate_43.jpg", nombre: "mate 43"},
+    { imagen:"/img/imagenes/Mate_44.jpg", nombre: "mate 44"},
+    { imagen:"/img/imagenes/Mate_45.jpg", nombre: "mate 45"},
+    { imagen:"/img/imagenes/Mate_46.jpg", nombre: "mate 46"},
+    { imagen:"/img/imagenes/Mate_47.jpg", nombre: "mate 47"},
+    { imagen:"/img/imagenes/Mate_48.jpg", nombre: "mate 48"},
+    { imagen:"/img/imagenes/Mate_49.jpg", nombre: "mate 48"},
+    { imagen:"/img/imagenes/Mate_50.jpg", nombre: "mate 49"},
+    { imagen:"/img/imagenes/Mate_51.jpg", nombre: "mate 49"},
+    { imagen:"/img/imagenes/Mate_52.jpg", nombre: "mate 50"},
+    { imagen:"/img/imagenes/Mate_53.jpg", nombre: "mate 51"},
+    { imagen:"/img/imagenes/Mate_54.jpg", nombre: "mate 52"},
+    { imagen:"/img/imagenes/Mate_55.jpg", nombre: "mate 53"},
+    { imagen:"/img/imagenes/Mate_56.jpg", nombre: "mate 54"},
+    { imagen:"/img/imagenes/Mate_57.jpg", nombre: "mate 55"},
+    { imagen:"/img/imagenes/Mate_58.jpg", nombre: "mate 56"},
+    { imagen:"/img/imagenes/Mate_59.jpg", nombre: "mate 57"},
+    { imagen:"/img/imagenes/Mate_60.jpg", nombre: "mate 58"},
+    { imagen:"/img/imagenes/Mate_61.jpg", nombre: "mate 59"},
+    { imagen:"/img/imagenes/Mate_62.jpg", nombre: "mate 60"},
+    { imagen:"/img/imagenes/Mate_63.jpg", nombre: "mate 61"},
+    { imagen:"/img/imagenes/Mate_64.jpg", nombre: "mate 62"},
+    { imagen:"/img/imagenes/Mate_65.jpg", nombre: "mate 63"},
+    { imagen:"/img/imagenes/Mate_66.jpg", nombre: "mate 64"},
+    { imagen:"/img/imagenes/Mate_67.jpg", nombre: "mate 65"},
+    { imagen:"/img/imagenes/Mate_68.jpg", nombre: "mate 66"},
+    { imagen:"/img/imagenes/Mate_69.jpg", nombre: "mate 67"},
+    { imagen:"/img/imagenes/Mate_70.jpg", nombre: "mate 68"},
+    { imagen:"/img/imagenes/Mate_71.jpg", nombre: "mate 69"},
+    { imagen:"/img/imagenes/Mate_72.jpg", nombre: "mate 70"},
+    { imagen:"/img/imagenes/Mate_73.jpg", nombre: "mate 71"},
+    { imagen:"/img/imagenes/Mate_74.jpg", nombre: "mate 72"},
+    { imagen:"/img/imagenes/Mate_75.jpg", nombre: "mate 73"},
+    { imagen:"/img/imagenes/Mate_76.jpg", nombre: "mate 74"},
+    { imagen:"/img/imagenes/Mate_77.jpg", nombre: "mate 75"},
+    { imagen:"/img/imagenes/Mate_78.jpg", nombre: "mate 76"},
+    { imagen:"/img/imagenes/Mate_79.jpg", nombre: "mate 77"},
+    { imagen:"/img/imagenes/Mate_80.jpg", nombre: "mate 78"},
+    { imagen:"/img/imagenes/Mate_81.jpg", nombre: "mate 79"},
+    { imagen:"/img/imagenes/Mate_82.jpg", nombre: "mate 80"},
+    { imagen:"/img/imagenes/Mate_83.jpg", nombre: "mate 81"},
+    { imagen:"/img/imagenes/Mate_84.jpg", nombre: "mate 82"},
+    { imagen:"/img/imagenes/Mate_85.jpg", nombre: "mate 83"}
+];
 
+console.log(mates)
 
-for (i = 0; i < mates.length; i++) {
-    //creacion del div "elementos"
+mates.map(articulo => {
     const divMate = document.createElement("div");
     divMate.classList.add("elemento");
 
     //creacion de los elementos del div
     const imgMate = document.createElement("img");
-    imgMate.src = mates[i][0];
+    imgMate.src = articulo.imagen;
     imgMate.classList.add("img-fluid");
 
-    const tituloMate= document.createElement("p");
-    tituloMate.textContent=mates[i][1];
+    const tituloMate = document.createElement("p");
+    tituloMate.textContent = articulo.nombre;
     tituloMate.classList.add("texto-imagen")
     //asignacion de los elementos al divArticulo
     divMate.appendChild(imgMate);
@@ -108,9 +109,9 @@ for (i = 0; i < mates.length; i++) {
 
     console.log(divMate);
 
-    const articulo = document.querySelector(".grilla");
-    articulo.appendChild(divMate);
+    const articulos = document.querySelector(".grilla");
+    articulos.appendChild(divMate);
+
+})
 
 
-
-}
